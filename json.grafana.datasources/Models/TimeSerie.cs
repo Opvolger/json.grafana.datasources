71b16654db8b5f4d60382f0ad03139305a55aa66
Bas Magré <Opvolger@gmail.com>
2019-10-24 14:12:44 +0200

    1 file vergeten.

--- json.grafana.datasources/Models/TimeSerie.cs
+++ json.grafana.datasources/Models/TimeSerie.cs
@@ -1,11 +1,14 @@
 ﻿namespace Json.Grafana.DataSources.Models
 {
     using System.Collections.Generic;
+    using Newtonsoft.Json;
 
     public class TimeSerie : QueryResponse
     {
+        [JsonProperty(PropertyName = "target")]
         public string Target { get; set; }
 
+        [JsonProperty(PropertyName = "datapoints")]
         public List<float[]> Datapoints { get; set; }
     }
 }
